4b3e26f2a07a2c1e8b45440d5291255269745d67
khaledshehab <62012519+khaledshehab@users.noreply.github.com>
2023-11-14 18:50:31 +0200

    Add files via upload

---
+++ FEE/FEE_Interface.h
@@ -0,0 +1,16 @@
+/*
+ * FEE_Interface.h
+ *
+ *  Created on: Nov 7, 2023
+ *      Author: Khaled shehab
+ */
+
+#ifndef FEE_INTERFACE_H_
+#define FEE_INTERFACE_H_
+
+/* Prototypes */
+void FEE_voidSetInit(void);
+void FEE_voidSetData(u16 data);
+u16 FEE_u16GetData(void);
+
+#endif
---
+++ FEE/FEE_Private.h
@@ -0,0 +1,13 @@
+/*
+ * FEE_Private.h
+ *
+ *  Created on: Nov 7, 2023
+ *      Author: Khaled shehab
+ */
+
+#ifndef FEE_PRIVATE_H_
+#define FEE_PRIVATE_H_
+
+
+
+#endif
